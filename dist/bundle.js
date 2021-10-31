cd49f6a6f93e3b273dc4112058734bb38f664a35
Gabriel Pereira Lazaro <gabrielpereiradev@outlook.com>
2021-10-31 01:57:06 -0300

    add babel for tradution react

---
+++ dist/bundle.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = App = function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello word");
+};
+
+exports["default"] = _default;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default App = ()=>{
+    return (
+        <h1>Hello word</h1>
+    )
+}
